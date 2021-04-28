ed1b8c05c78cded992a78e7b9cc2ba415733e10e
Гизетдинов Искандер <gizetiskander@gmail.com>
2021-04-28 13:49:00 +0300

    I added combobox

--- Form1/Form1.cs
+++ Form1/Form1.cs
@@ -21,13 +21,43 @@ namespace Form1
         {
             var graphics = panel1.CreateGraphics();
             Pen pn = new Pen(Color.Black, 5);
-            graphics.DrawLine(pn, 50, 50, 50, 50);
-            graphics.DrawEllipse(pn, 50, 50, 50, 50);
-        }
+            string combo = comboBox1.Text;
+            if (combo == "Отрезок")
+            {
+                graphics.DrawLine(pn, 10, 10, 100, 50);
+            }
 
+            if (combo == "Круг")
+            {
+                graphics.DrawEllipse(pn, 50, 50, 50, 50);
+            }
+
+            if (combo == "Прямоугольник")
+            {
+                graphics.DrawRectangle(pn, 50, 50, 50, 50);
+            }
+
+            if (combo == "Треугольник")
+            {
+                Point point1 = new Point(20, 20);
+                Point point2 = new Point(100, 150);
+                Point point3 = new Point(20, 150);
+                Point[] curvePoints =
+             {
+                 point1, point2, point3,
+             };
+                graphics.DrawPolygon(pn, curvePoints);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var graphics = panel1.CreateGraphics();
+            graphics.Clear(Color.White);
+        }
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
-    }
+
+        }
 }
